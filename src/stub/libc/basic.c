d04d25550b799d2650d5165d670ca33a42c1b072
omer <omer@UsainBolt.localdomain>
2020-03-23 23:23:19 +0200

    added generic libc stuff

---
+++ src/stub/libc/basic.c
@@ -0,0 +1,60 @@
+#include <stdint.h>
+#include <string.h>
+
+void *memcpy(void *dst, const void *src, size_t n)
+{
+    const char *p = src;
+    char *q = dst;
+    while (n--) {
+            *q++ = *p++;
+        }
+    return dst;
+}
+
+void *memset(void *dst, int c, size_t n)
+{
+    char *q = dst;
+    while (n--) {
+            *q++ = c;
+        }
+    return dst;
+}
+
+size_t strlen(const char *s)
+{
+    const char *ss = s;
+    while (*ss)
+        ss++;
+    return ss - s;
+}
+
+char *strcpy(char *dst, const char *src)
+{
+    char *q = dst;
+    const char *p = src;
+    char ch;
+
+    do {
+            *q++ = ch = *p++;
+        } while (ch);
+
+    return dst;
+}
+
+char *strchr(const char *s, int c)
+{
+    while (*s != (char)c) {
+            if (!*s)
+                return NULL;
+            s++;
+        }
+
+    return (char *)s;
+}
+
+char *strcat(char *dst, const char *src)
+{
+    strcpy(strchr(dst, '\0'), src);
+    return dst;
+}
+
