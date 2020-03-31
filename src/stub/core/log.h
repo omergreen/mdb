d2de84b4eda27c2ad41accea8058968a38f1f849
Omer Green <omer@omer.green>
2020-03-31 10:15:36 -0500

    add colors to log

--- src/stub/core/log.h
+++ src/stub/core/log.h
@@ -2,14 +2,37 @@
 
 #include <core/ops.h>
 
-#define _MESSAGE(s, type, ...) g_ops.log("%s %s:%d: " s "\n", type, __FILE__, __LINE__, ##__VA_ARGS__)
-
+#define COLORS
 #define DEBUGMODE
+
+#ifdef COLORS
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#else
+#define KNRM  ""
+#define KRED  ""
+#define KGRN  ""
+#define KYEL  ""
+#define KBLU  ""
+#define KMAG  ""
+#define KCYN  ""
+#define KWHT  ""
+#endif
+
+
+#define _MESSAGE(s, type, color, ...) g_ops.log("%s%s%s %s:%d: %s" s "%s\n", color, type, KNRM, __FILE__, __LINE__, color, ##__VA_ARGS__, KNRM)
+
 #ifdef DEBUGMODE
-#define DEBUG(s, ...) _MESSAGE(s, "DEBUG", ##__VA_ARGS__)
+#define DEBUG(s, ...) _MESSAGE(s, "DEBUG", KYEL, ##__VA_ARGS__)
 #else
 #define DEBUG(S, ...)
 #endif
 
-#define ERROR(s, ...) _MESSAGE(s, "ERROR", ##__VA_ARGS__)
+#define ERROR(s, ...) _MESSAGE(s, "ERROR", KRED, ##__VA_ARGS__)
 
