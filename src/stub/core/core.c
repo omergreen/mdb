0f126f3342ac647dc14050cf58b240d9508a8e46
Omer Green <omer@omer.green>
2020-04-28 08:01:14 -0500

    fix core and linker.ld in preparation for mips

--- src/stub/core/core.c
+++ src/stub/core/core.c
@@ -15,20 +15,61 @@ extern unsigned long __START_OFFSET;
 
 struct state g_state = { .state = STATE_CONTINUE, .previous_pc_breakpoint = 0 };
 
-void fix_got();
+static void fix_got();
 
+#ifdef ARCH_MIPS
+// per abicalls, _start2 expects that t9 will point to the start of the function
+asm(".global _start;\
+     .section .init;\
+     .set noreorder;\
+_start:\
+     move $t0, $ra;\
+     bal 1f;\
+     addiu $t9, $ra, 8;\
+\
+  1: b _start2;\
+     move $ra, $t0;\
+    ");
+extern void *_start;
+__attribute__((used,section(".init"))) void _start2(void *args) {
+#else
 __attribute__((used,section(".init"))) void _start(void *args) {
+#endif
     fix_got();
     arch_init();
     target_init(args);
 }
 
-void fix_got() {
-    unsigned long start = (unsigned long)&_start;
-    unsigned long *got = (unsigned long *)(start + (unsigned long)&__GOT_OFFSET); // stackoverflow.com/q/8398755
-    for (unsigned int i = 0; i < ((unsigned long)&__GOT_LENGTH) / sizeof(*got); ++i) {
+__attribute__((always_inline,section(".init"))) static void fix_got() {
+    register unsigned long start, got_offset, got_length, start_offset;
+#ifdef ARCH_MIPS
+    asm volatile(".set noreorder; bal 1f; addiu %0, $ra, _start - . - 4; 1:" :: "r" (start));
+/*     // we have to load the various linker symbols ourselves since gcc insists on loading */
+/*     // them through the GOT */
+/*     asm volatile(".set noreorder;\ */
+/*                   bal 1f;\ */
+/*                   lw %0, 16($ra);\ */
+/*                   \ */
+/*                1: addiu %3, $ra, _start - .;\ */
+/*                   lw %1, 20($ra);\ */
+/*                   b 1f;\ */
+/*                   lw %2, 24($ra);\ */
+/*                   got_offset: .word __GOT_OFFSET;\ */
+/*                   got_length: .word __GOT_LENGTH;\ */
+/*                   start_offset: .word __START_OFFSET;\ */
+/*                 1:\ */
+/*                 " :: "r" (got_offset), "r" (got_length), "r" (start_offset), "r" (start) */
+/*                    : "ra"); */
+#else
+    start = (unsigned long)&_start; // stackoverflow.com/q/8398755
+#endif
+    got_offset = (unsigned long)&__GOT_OFFSET;
+    got_length = (unsigned long)&__GOT_LENGTH;
+    start_offset = (unsigned long)&__START_OFFSET;
+    unsigned long *got = (unsigned long *)(start + got_offset);
+    for (unsigned int i = 0; i < got_length / sizeof(*got); ++i) {
         if ((got[i] & 0xfff00000) == 0x12300000) {
-            got[i] += start - (unsigned long)&__START_OFFSET - 0x12300000;
+            got[i] += start - start_offset - 0x12300000;
         }
     }
 }
