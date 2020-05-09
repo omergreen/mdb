8b363a9024790f663062559aef6ce90601b10646
Omer Green <omer@omer.green>
2020-05-09 10:43:40 -0500

    more work on kernel, still not working

--- src/stub/target/linux_kernel/kernel_module.c
+++ src/stub/target/linux_kernel/kernel_module.c
@@ -1,19 +1,70 @@
 #include <linux/init.h>
+#include <asm/pgtable.h>
+#include <linux/set_memory.h>
+#include <asm/cacheflush.h>
+#include <linux/uaccess.h>
+#include <linux/moduleloader.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-void *malloc(size_t size) {
-    return kmalloc(size, GFP_ATOMIC); 
-}
+#include <stub.h>
 
-static int __init lkm_example_init(void) {
-    printk(KERN_INFO "Hello, World!\n");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("green");
+MODULE_DESCRIPTION("mom");
+MODULE_VERSION("13.37");
+
+
+int serial_write(const void* buffer, int size) {
     return 0;
 }
-static void __exit lkm_example_exit(void) {
-    printk(KERN_INFO "Goodbye, World!\n");
+int serial_read(void* buffer, int size) {
+    return 0;
+}                                                                                                                                                                                     
+
+void *big_alloc(size_t size) {
+    void *p = kmalloc(size, GFP_KERNEL);
+    set_memory_x(p, size / 1024);
+    return (void *)((unsigned long)p | 0x20000000);
+    /* return __vmalloc(size, GFP_KERNEL, PAGE_KERNEL_EXEC); */
+    /* return module_alloc(size); */
+    	/* return __vmalloc_node_range(size, 1, MODULE_START, MODULE_END, */
+				/* GFP_KERNEL, PAGE_KERNEL, 0, NUMA_NO_NODE, */
+				/* __builtin_return_address(0)); */
+    /* return vmalloc_exec(size); */
+}
+void printf(char *s) {
+    printk(s);
 }
 
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+void test2(void) {
+}
+
+static int __init init(void) {
+    unsigned long args[] = { (unsigned long)&serial_read, (unsigned long)&serial_write, (unsigned long)&printf, (unsigned long)&big_alloc, (unsigned long)&test2 };
+    void *new_addr;
+    mm_segment_t old_fs;
+
+    printk(KERN_INFO "init kernel module\n");
+
+    new_addr = big_alloc(stub_bin_len);
+    memcpy(new_addr, stub_bin, stub_bin_len);
+
+    old_fs = get_fs();
+	set_fs(KERNEL_DS);
+    flush_icache_range((unsigned long)new_addr, (unsigned long)new_addr + stub_bin_len);
+    set_fs(old_fs);
+
+    ((void (*)(void *))new_addr)(args);
+
+    return 0;
+}
+static void __exit exit(void) {
+    printk(KERN_INFO "");
+}
+
+module_init(init);
+module_exit(exit);
 
--- src/stub/target/linux_kernel/linux_kernel.c
+++ src/stub/target/linux_kernel/linux_kernel.c
@@ -2,12 +2,13 @@
 #include <core/breakpoint.h>
 #include <stdarg.h>
 #include <libc/libc.h>
+#include <libc/malloc.h>
 
 struct target_config g_target_config = { .supports_real_breakpoints = true, .should_override_ivt = true };
 
 int (*serial2_read)(char *buffer, unsigned int length);
 int (*serial2_write)(const char *buffer, unsigned int length);
-int (*vprintf)(const char *format, va_list args);
+void (*puts)(const char *s);
 void *(*_malloc)(unsigned int size);
 void *(*_realloc)(void *ptr, unsigned int size);
 void (*_free)(void *addr);
@@ -46,30 +47,20 @@ unsigned int target_send(const char *data, unsigned int length) {
 }
 void target_log(const char *format, ...) {
     char buf[4096];
-#ifdef ARCH_ARM
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-#elif ARCH_MIPS
-    // qemu mipssim only has one serial port so we have to multiplex over it
     va_list args;
     va_start(args, format);
     tfp_vsnprintf(buf, sizeof(buf), format, args);
     va_end(args);
-    target_send(buf, strlen(buf));
-#else
-#error "not implemented"
-#endif
+    puts(buf);
 }
 void *target_malloc(unsigned int size) {
-    return _malloc(size);
+    return malloc(size);
 }
 void *target_realloc(void *ptr, unsigned int size) {
-    return _realloc(ptr, size);
+    return realloc(ptr, size);
 }
 void target_free(void *addr) {
-    _free(addr);
+    free(addr);
 }
 
 void wait_for_connection() {
@@ -82,12 +73,18 @@ void target_init(void *args) {
     void **args_ptr = args;
     serial2_read = args_ptr[0];
     serial2_write = args_ptr[1];
-    vprintf = args_ptr[2];
-    _malloc = args_ptr[3];
-    _realloc = args_ptr[4];
-    _free = args_ptr[5];
+    puts = args_ptr[2];
+    void *(*big_alloc)(size_t) = args_ptr[3];
 
-    breakpoint_add((unsigned long)args_ptr[6], false, BREAKPOINT_TYPE_SOFTWARE);
+    /* DEBUG("allocing for malloc"); */
+    puts("test\n");
+    return;
+    malloc_init();
+    /* void *malloc_block = big_alloc(0x10000); */
+    DEBUG("malloc_block = 0x%08x", 123);
+    /* add_malloc_block(malloc_block, 0x10000); */
+
+    /* breakpoint_add((unsigned long)args_ptr[6], false, BREAKPOINT_TYPE_SOFTWARE); */
 
     wait_for_connection();
 }
